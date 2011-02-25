1300d896d9b16ac35d00b6692e16547c48c53050
Daniel Mendler <mail@daniel-mendler.de>
2011-02-25 17:04:02 +0100

    remove lines with length zero

--- mapgen.sh
+++ mapgen.sh
@@ -53,36 +53,40 @@ rm -f $DEST_PATH/*
 mkdir -p $DEST_PATH
 cp $CONVERTED_PATH/*_point* $DEST_PATH
 
-g.remove vect=roadbig_line,roadbig_line1,roadbig_line2,roadbig_line3
+g.remove vect=roadbig_line,roadbig_line1,roadbig_line2,roadbig_line3,roadbig_line4
 v.in.ogr -t dsn=$CONVERTED_PATH layer=roadbig_line output=roadbig_line1
 v.build.polylines input=roadbig_line1 output=roadbig_line2
 v.generalize input=roadbig_line2 output=roadbig_line3 method=douglas threshold=0.002
-v.clean input=roadbig_line3 output=roadbig_line tool=snap,break,rmdupl thres=0.002
-g.remove vect=roadbig_line1,roadbig_line2,roadbig_line3
+v.clean input=roadbig_line3 output=roadbig_line4 tool=snap,break,rmdupl thres=0.002
+v.clean input=roadbig_line4 output=roadbig_line tool=rmline type=line
+g.remove vect=roadbig_line1,roadbig_line2,roadbig_line3,roadbig_line4
 v.out.ogr input=roadbig_line type=line dsn=$DEST_PATH/roadbig_line.shp
 
-g.remove vect=roadmedium_line,roadmedium_line1,roadmedium_line2,roadmedium_line3
+g.remove vect=roadmedium_line,roadmedium_line1,roadmedium_line2,roadmedium_line3,roadmedium_line4
 v.in.ogr -t dsn=$CONVERTED_PATH layer=roadmedium_line output=roadmedium_line1
 v.build.polylines input=roadmedium_line1 output=roadmedium_line2
 v.generalize input=roadmedium_line2 output=roadmedium_line3 method=douglas threshold=0.002
-v.clean input=roadmedium_line3 output=roadmedium_line tool=snap,break,rmdupl thres=0.002
-g.remove vect=roadmedium_line1,roadmedium_line2,roadmedium_line3
+v.clean input=roadmedium_line3 output=roadmedium_line4 tool=snap,break,rmdupl thres=0.002
+v.clean input=roadmedium_line4 output=roadmedium_line tool=rmline type=line
+g.remove vect=roadmedium_line1,roadmedium_line2,roadmedium_line3,roadmedium_line4
 v.out.ogr input=roadmedium_line type=line dsn=$DEST_PATH/roadmedium_line.shp
 
-g.remove vect=roadsmall_line,roadsmall_line1,roadsmall_line2,roadsmall_line3
+g.remove vect=roadsmall_line,roadsmall_line1,roadsmall_line2,roadsmall_line3,roadsmall_line4
 v.in.ogr -t dsn=$CONVERTED_PATH layer=roadsmall_line output=roadsmall_line1
 v.build.polylines input=roadsmall_line1 output=roadsmall_line2
 v.generalize input=roadsmall_line2 output=roadsmall_line3 method=douglas threshold=0.005
-v.clean input=roadsmall_line3 output=roadsmall_line tool=snap,break,rmdupl thres=0.005
-g.remove vect=roadsmall_line1,roadsmall_line2,roadsmall_line3
+v.clean input=roadsmall_line3 output=roadsmall_line4 tool=snap,break,rmdupl thres=0.005
+v.clean input=roadsmall_line4 output=roadsmall_line tool=rmline type=line
+g.remove vect=roadsmall_line1,roadsmall_line2,roadsmall_line3,roadsmall_line4
 v.out.ogr input=roadsmall_line type=line dsn=$DEST_PATH/roadsmall_line.shp
 
-g.remove vect=railway_line,railway_line1,railway_line2,railway_line3
+g.remove vect=railway_line,railway_line1,railway_line2,railway_line3,railway_line4
 v.in.ogr -t dsn=$CONVERTED_PATH layer=railway_line output=railway_line1
 v.build.polylines input=railway_line1 output=railway_line2
 v.generalize input=railway_line2 output=railway_line3 method=douglas threshold=0.002
-v.clean input=railway_line3 output=railway_line tool=snap,break,rmdupl thres=0.002
-g.remove vect=railway_line1,railway_line2,railway_line3
+v.clean input=railway_line3 output=railway_line4 tool=snap,break,rmdupl thres=0.002
+v.clean input=railway_line4 output=railway_line tool=rmline type=line
+g.remove vect=railway_line1,railway_line2,railway_line3,railway_line4
 v.out.ogr input=railway_line type=line dsn=$DEST_PATH/railway_line.shp
 
 echo "================================================================================"
